3dab37701fde2a50880688d77c8d62cdb8aa4643
Kiet Tuan <kietops365@gmail.com>
2025-02-21 23:19:20 +0700

    Add @CrossOrigin in SettingController
    Signed-off-by: kit365 <kietops@gmail.com>

--- src/main/java/com/kit/maximus/freshskinweb/controller/SettingController.java
+++ src/main/java/com/kit/maximus/freshskinweb/controller/SettingController.java
@@ -12,7 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
