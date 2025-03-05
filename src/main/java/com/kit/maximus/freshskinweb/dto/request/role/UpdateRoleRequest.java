6e355722b587860dabb882defb60b98c3fb8e364
Doan Minh Phu <phujason1992@gmail.com>
2025-03-05 20:38:29 +0700

    Update: user and role, rollback to initial Entity

--- src/main/java/com/kit/maximus/freshskinweb/dto/request/role/UpdateRoleRequest.java
+++ src/main/java/com/kit/maximus/freshskinweb/dto/request/role/UpdateRoleRequest.java
@@ -1,5 +1,6 @@
 package com.kit.maximus.freshskinweb.dto.request.role;
 
+import com.kit.maximus.freshskinweb.entity.AbstractEntity;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -9,9 +10,11 @@ import lombok.experimental.FieldDefaults;
 @AllArgsConstructor
 @ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class UpdateRoleRequest {
+public class UpdateRoleRequest  {
     Long roleId;
     String title;
     String description;
     String permission;
+    String status;
+
 }
