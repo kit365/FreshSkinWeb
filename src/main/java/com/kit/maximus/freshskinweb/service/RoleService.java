8c76cf6493df91971f751bccef953c6b873e57d1
Phuoc <phuocnguyenhuu2510@gmail.com>
2025-03-08 16:51:43 +0700

    fix: Update VNPAY configuration properties and clear role repository before saving

--- src/main/java/com/kit/maximus/freshskinweb/service/RoleService.java
+++ src/main/java/com/kit/maximus/freshskinweb/service/RoleService.java
@@ -70,6 +70,7 @@ public class RoleService implements BaseService<RoleResponseDTO, CreateRoleReque
                     });
         });
 
+        roleRepository.deleteAll();
         roleRepository.saveAll(roles);
         return true;
     }
