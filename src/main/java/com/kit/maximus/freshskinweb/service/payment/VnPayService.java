a6031e0f0f1931a47a1416469f8871b2e1285d9a
kit365 <kietops@gmail.com>
2025-03-12 02:06:47 +0700

    refactor: Change amount type from double to long in VnPayService for precision
    Signed-off-by: kit365 <kietops@gmail.com>

--- src/main/java/com/kit/maximus/freshskinweb/service/payment/VnPayService.java
+++ src/main/java/com/kit/maximus/freshskinweb/service/payment/VnPayService.java
@@ -80,7 +80,7 @@ public class VnPayService implements PaymentService {
             String version = "2.1.0";
             String command = "pay";
             String otherType = "130000";
-            double amount = order.getTotalPrice() * 100; // Format tiền theo cent
+            long amount = (long) (order.getTotalPrice() * 100);; // Format tiền theo cent
             String transactionReference = getRandomNumber(8);
             String clientIpAddress = getIpAddress(ipAddr);
 //            String clientIpAddress = "127.0.0.1"; //localhost
