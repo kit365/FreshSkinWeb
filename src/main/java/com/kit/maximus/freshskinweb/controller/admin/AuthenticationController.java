5ff04637980ff7bb57388ff889a8bda4ca81ede8
Kiet Tuan <kietops365@gmail.com>
2025-03-12 04:18:21 +0700

    refactor: Enhance authentication flow by adding HttpServletRequest to login method and adjusting cookie settings based on server name
    Signed-off-by: kit365 <kietops@gmail.com>

--- src/main/java/com/kit/maximus/freshskinweb/controller/admin/AuthenticationController.java
+++ src/main/java/com/kit/maximus/freshskinweb/controller/admin/AuthenticationController.java
@@ -10,17 +10,20 @@ import com.kit.maximus.freshskinweb.dto.response.ResponseAPI;
 import com.kit.maximus.freshskinweb.dto.response.UserResponseDTO;
 import com.kit.maximus.freshskinweb.service.AuthenticationService;
 import com.nimbusds.jose.JOSEException;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
 //@CrossOrigin(origins = "*")
+@Slf4j
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 @RestController
@@ -31,9 +34,10 @@ public class AuthenticationController {
     AuthenticationService authenticationService;
 
     @PostMapping("/login")
-    public ResponseAPI<AuthenticationResponseDTO> checkLogin(@RequestBody AuthenticationRequest request, HttpServletResponse response) {
+    public ResponseAPI<AuthenticationResponseDTO> checkLogin(@RequestBody AuthenticationRequest request, HttpServletResponse response, HttpServletRequest httpRequest ) {
+        log.info(httpRequest.getRequestURI());
         String message = "Đăng nhập thành công";
-        AuthenticationResponseDTO result = authenticationService.authenticate(request, response);
+        AuthenticationResponseDTO result = authenticationService.authenticate(request, response,httpRequest);
 
         return ResponseAPI.<AuthenticationResponseDTO>builder().code(HttpStatus.OK.value()).message(message).data(result).build();
 
