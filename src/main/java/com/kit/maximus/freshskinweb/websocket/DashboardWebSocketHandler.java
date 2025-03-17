25a8994c730bd0e31a068bde5949ca63993723b1
Kiet Tuan <kietops365@gmail.com>
2025-03-17 11:51:38 +0700

    feat: Refactor DashboardWebSocketHandler for improved session management and data handling
    Signed-off-by: kit365 <kietops@gmail.com>

--- src/main/java/com/kit/maximus/freshskinweb/websocket/DashboardWebSocketHandler.java
+++ src/main/java/com/kit/maximus/freshskinweb/websocket/DashboardWebSocketHandler.java
@@ -1,11 +1,133 @@
+//package com.kit.maximus.freshskinweb.websocket;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.kit.maximus.freshskinweb.dto.response.ProductResponseDTO;
+//import com.kit.maximus.freshskinweb.service.DashboardService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.jetbrains.annotations.NotNull;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.socket.CloseStatus;
+//import org.springframework.web.socket.TextMessage;
+//import org.springframework.web.socket.WebSocketSession;
+//import org.springframework.web.socket.handler.TextWebSocketHandler;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.concurrent.CompletableFuture;
+//
+//@Slf4j
+//@Component
+//public class DashboardWebSocketHandler extends TextWebSocketHandler {
+//
+//    @Autowired
+//    private DashboardService dashboardService;
+//
+//    private List<WebSocketSession> sessions = new ArrayList<>();
+//    // Dữ liệu cũ để so sánh để tránh spam quá niều
+//    private Map<String, Object> previousData = new HashMap<>();
+//
+//    @Override
+//    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+//        System.out.println("📩 Kết nối cổng Dashboard mới từ:" + session.getId());
+//        sessions.add(session);
+//
+//        //gửi dữ liệu khi connect
+//        sendDataDashBoard(session);
+//
+//    }
+//
+//    @Override
+//    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+//        System.out.println("❌ Kết nối đóng từ cổng dashboard: " + session.getId());
+//        sessions.remove(session);
+//        previousData.clear();
+//    }
+//
+////     Gửi dữ liệu tự động mỗi 5s
+//    @Scheduled(fixedRate = 5000) // Gửi mỗi 5s)
+//    public void sendPeriodicDataToClients() {
+//        for (WebSocketSession session : sessions) {
+//                sendDataDashBoard(session);
+//        }
+//    }
+//
+//    public void sendDataDashBoard(WebSocketSession session) {
+//        // Lấy dữ liệu mới từ các service
+//        CompletableFuture<String> totalRevenue = dashboardService.getTotalRevenue();
+//        CompletableFuture<Long> totalOrderCompleted = dashboardService.getOrderCompleted();
+//        CompletableFuture<Long> totalOrderPending = dashboardService.getOrderPending();
+//        CompletableFuture<Long> totalOrderCanceled = dashboardService.getOrderCanceled();
+//        CompletableFuture<Long> totalOrder = dashboardService.getTotalOrder();
+//        CompletableFuture<Long> totalBlogs = dashboardService.getTotalBlogs();
+//        CompletableFuture<Long> totalFeedback = dashboardService.getTotalReviews();
+//        CompletableFuture<Long> totalUser = dashboardService.getTotalUsers();
+//        CompletableFuture<Long> totalProduct = dashboardService.getTotalProducts();
+//        CompletableFuture<List<ProductResponseDTO>> top10ProductSelling = dashboardService.getTop10SellingProducts();
+//
+//        CompletableFuture.allOf(
+//                totalRevenue,
+//                totalOrderCompleted,
+//                totalOrderPending,
+//                totalOrderCanceled,
+//                totalOrder,
+//                totalBlogs,
+//                totalFeedback,
+//                totalUser,
+//                totalProduct,
+//                top10ProductSelling
+//        ).join();
+//
+//        Map<String, Object> data = new HashMap<>();
+//        try {
+//            // Đặt giá trị vào map
+//            data.put("totalOrder", totalOrder.get());
+//            data.put("totalOrderCompleted", totalOrderCompleted.get());
+//            data.put("totalOrderPending", totalOrderPending.get());
+//            data.put("totalOrderCanceled", totalOrderCanceled.get());
+//            data.put("totalRevenue", totalRevenue.get());
+//            data.put("totalBlogs", totalBlogs.get());
+//            data.put("totalFeedbacks", totalFeedback.get());
+//            data.put("totalUsers", totalUser.get());
+//            data.put("totalProducts", totalProduct.get());
+//            data.put("top10ProductSelling", top10ProductSelling.get());
+//
+//            // Chuyển dữ liệu mới và cũ thành JSON
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            String previousJson = objectMapper.writeValueAsString(previousData);
+//            String newJson = objectMapper.writeValueAsString(data);
+//
+//            // Kiểm tra xem dữ liệu có thay đổi không
+//            if (!previousJson.equals(newJson)) {
+//                // Nếu có thay đổi, gửi qua WebSocket
+//                String jsonData = new ObjectMapper().writeValueAsString(data);
+//                session.sendMessage(new TextMessage(jsonData));
+//
+//                // Cập nhật dữ liệu cũ
+//                previousData = new HashMap<>(data);
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//
+//
+//
+//}
+
+
+
 package com.kit.maximus.freshskinweb.websocket;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kit.maximus.freshskinweb.dto.response.ProductResponseDTO;
 import com.kit.maximus.freshskinweb.service.DashboardService;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -14,7 +136,6 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -27,64 +148,50 @@ public class DashboardWebSocketHandler extends TextWebSocketHandler {
     @Autowired
     private DashboardService dashboardService;
 
-    private List<WebSocketSession> sessions = new ArrayList<>();
-    // Dữ liệu cũ để so sánh để tránh spam quá niều
+    private Map<String, WebSocketSession> sessions = new HashMap<>();
     private Map<String, Object> previousData = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println("📩 Kết nối cổng Dashboard mới từ:" + session.getId());
-        sessions.add(session);
-
-        //gửi dữ liệu khi connect
+        log.info("📩 Kết nối mới từ Dashboard: {}", session.getId());
+        sessions.put(session.getId(), session);
         sendDataDashBoard(session);
-
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-        System.out.println("❌ Kết nối đóng từ cổng dashboard: " + session.getId());
-        sessions.remove(session);
-        previousData.clear();
+        log.info("❌ Kết nối đóng từ Dashboard: {}", session.getId());
+        sessions.remove(session.getId());
     }
 
-//     Gửi dữ liệu tự động mỗi 5s
-    @Scheduled(fixedRate = 5000) // Gửi mỗi 5s)
+    @Scheduled(fixedRate = 5000)
     public void sendPeriodicDataToClients() {
-        for (WebSocketSession session : sessions) {
+        for (WebSocketSession session : sessions.values()) {
+            if (session.isOpen()) {
                 sendDataDashBoard(session);
+            }
         }
     }
 
     public void sendDataDashBoard(WebSocketSession session) {
-        // Lấy dữ liệu mới từ các service
-        CompletableFuture<String> totalRevenue = dashboardService.getTotalRevenue();
-        CompletableFuture<Long> totalOrderCompleted = dashboardService.getOrderCompleted();
-        CompletableFuture<Long> totalOrderPending = dashboardService.getOrderPending();
-        CompletableFuture<Long> totalOrderCanceled = dashboardService.getOrderCanceled();
-        CompletableFuture<Long> totalOrder = dashboardService.getTotalOrder();
-        CompletableFuture<Long> totalBlogs = dashboardService.getTotalBlogs();
-        CompletableFuture<Long> totalFeedback = dashboardService.getTotalReviews();
-        CompletableFuture<Long> totalUser = dashboardService.getTotalUsers();
-        CompletableFuture<Long> totalProduct = dashboardService.getTotalProducts();
-        CompletableFuture<List<ProductResponseDTO>> top10ProductSelling = dashboardService.getTop10SellingProducts();
-
-        CompletableFuture.allOf(
-                totalRevenue,
-                totalOrderCompleted,
-                totalOrderPending,
-                totalOrderCanceled,
-                totalOrder,
-                totalBlogs,
-                totalFeedback,
-                totalUser,
-                totalProduct,
-                top10ProductSelling
-        ).join();
-
-        Map<String, Object> data = new HashMap<>();
         try {
-            // Đặt giá trị vào map
+            CompletableFuture<String> totalRevenue = dashboardService.getTotalRevenue();
+            CompletableFuture<Long> totalOrderCompleted = dashboardService.getOrderCompleted();
+            CompletableFuture<Long> totalOrderPending = dashboardService.getOrderPending();
+            CompletableFuture<Long> totalOrderCanceled = dashboardService.getOrderCanceled();
+            CompletableFuture<Long> totalOrder = dashboardService.getTotalOrder();
+            CompletableFuture<Long> totalBlogs = dashboardService.getTotalBlogs();
+            CompletableFuture<Long> totalFeedback = dashboardService.getTotalReviews();
+            CompletableFuture<Long> totalUser = dashboardService.getTotalUsers();
+            CompletableFuture<Long> totalProduct = dashboardService.getTotalProducts();
+            CompletableFuture<List<ProductResponseDTO>> top10ProductSelling = dashboardService.getTop10SellingProducts();
+
+            CompletableFuture.allOf(
+                    totalRevenue, totalOrderCompleted, totalOrderPending, totalOrderCanceled,
+                    totalOrder, totalBlogs, totalFeedback, totalUser, totalProduct, top10ProductSelling
+            ).join();
+
+            Map<String, Object> data = new HashMap<>();
             data.put("totalOrder", totalOrder.get());
             data.put("totalOrderCompleted", totalOrderCompleted.get());
             data.put("totalOrderPending", totalOrderPending.get());
@@ -96,26 +203,16 @@ public class DashboardWebSocketHandler extends TextWebSocketHandler {
             data.put("totalProducts", totalProduct.get());
             data.put("top10ProductSelling", top10ProductSelling.get());
 
-            // Chuyển dữ liệu mới và cũ thành JSON
             ObjectMapper objectMapper = new ObjectMapper();
             String previousJson = objectMapper.writeValueAsString(previousData);
             String newJson = objectMapper.writeValueAsString(data);
 
-            // Kiểm tra xem dữ liệu có thay đổi không
             if (!previousJson.equals(newJson)) {
-                // Nếu có thay đổi, gửi qua WebSocket
-                String jsonData = new ObjectMapper().writeValueAsString(data);
-                session.sendMessage(new TextMessage(jsonData));
-
-                // Cập nhật dữ liệu cũ
+                session.sendMessage(new TextMessage(newJson));
                 previousData = new HashMap<>(data);
             }
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            log.error("🚨 Lỗi khi gửi dữ liệu WebSocket: {}", e.getMessage());
         }
     }
-
-
-
-
 }
