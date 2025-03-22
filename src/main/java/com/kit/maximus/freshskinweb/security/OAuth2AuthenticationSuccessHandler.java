aa7a97653893a9a9c3f0f8f8796e6dc84ba9eca7
Kiet Tuan <kietops365@gmail.com>
2025-03-22 17:18:43 +0700

    feat: Update OAuth2 authentication success handler to redirect with token instead of returning JSON response
    Signed-off-by: kit365 <kietops@gmail.com>

--- src/main/java/com/kit/maximus/freshskinweb/security/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/kit/maximus/freshskinweb/security/OAuth2AuthenticationSuccessHandler.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kit.maximus.freshskinweb.dto.response.AuthenticationResponseDTO;
 import com.kit.maximus.freshskinweb.dto.response.ResponseAPI;
 import com.kit.maximus.freshskinweb.service.users.AuthenticationService;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.AccessLevel;
@@ -17,6 +18,9 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
@@ -26,6 +30,28 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
       AuthenticationService authenticationService;
       ObjectMapper objectMapper;
 
+//    @Override
+//    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+//        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+//        String email = oAuth2User.getAttribute("email");
+//
+//        String token = authenticationService.generateToken(email);
+//
+//        AuthenticationResponseDTO authResponse = AuthenticationResponseDTO.builder()
+//                .token(token)
+//                .authenticated(true)
+//                .build();
+//
+//        ResponseAPI<AuthenticationResponseDTO> apiResponse = ResponseAPI.<AuthenticationResponseDTO>builder()
+//                .data(authResponse)
+//                .code(HttpStatus.OK.value())
+//                .message("Login successful")
+//                .build();
+//
+//        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+//        objectMapper.writeValue(response.getWriter(), apiResponse);
+//    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
@@ -33,18 +59,8 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 
         String token = authenticationService.generateToken(email);
 
-        AuthenticationResponseDTO authResponse = AuthenticationResponseDTO.builder()
-                .token(token)
-                .authenticated(true)
-                .build();
-
-        ResponseAPI<AuthenticationResponseDTO> apiResponse = ResponseAPI.<AuthenticationResponseDTO>builder()
-                .data(authResponse)
-                .code(HttpStatus.OK.value())
-                .message("Login successful")
-                .build();
-
-        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-        objectMapper.writeValue(response.getWriter(), apiResponse);
+        // Redirect FE với token
+        String redirectUrl = "https://project-swp391-n9j6.onrender.com?token=" + token;
+        response.sendRedirect(redirectUrl);
     }
 }
