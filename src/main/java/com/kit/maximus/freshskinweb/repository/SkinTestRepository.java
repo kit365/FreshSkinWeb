817de81afa5ff655a4a29305c4213aa01ef29046
PhuJs412 <phujason1992@gmail.com>
2025-03-31 17:58:08 +0700

    feat: Add skin type counting query to SkinTestRepository and update related classes

--- src/main/java/com/kit/maximus/freshskinweb/repository/SkinTestRepository.java
+++ src/main/java/com/kit/maximus/freshskinweb/repository/SkinTestRepository.java
@@ -4,9 +4,17 @@ import com.kit.maximus.freshskinweb.entity.SkinTestEntity;
 import com.kit.maximus.freshskinweb.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SkinTestRepository extends JpaRepository<SkinTestEntity, Long>, JpaSpecificationExecutor<SkinTestEntity> {
     SkinTestEntity findByUser(UserEntity user);
+
+    @Query("SELECT st.skinType.type as skinType, COUNT(st) as count " +
+            "FROM SkinTestEntity st " +
+            "GROUP BY st.skinType.type")
+    List<Object[]> countBySkinTypes();
 }
